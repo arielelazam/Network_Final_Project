4c27a0173d80d05d7110a09d103689c91ef431f3
pinhaszanzuri <pinhas.zanzuri@msmail.ariel.ac.il>
2026-02-10 12:36:05 +0200

    socket server

--- DHCP_Server.py
+++ DHCP_Server.py
@@ -2,6 +2,9 @@
 import json, socket
 from typing import Dict, Any
 
+DHCP_IP = "127.0.0.1"
+DHCP_PORT = 6767
+
 
 # פונקציה שמקבלת דיקשנרי הופכת אותו לגיסון וממירה אותו לבתים
 def encode(msg: Dict[str,Any]) -> bytes:
@@ -11,3 +14,12 @@ def encode(msg: Dict[str,Any]) -> bytes:
 def decode(data: bytes) -> dict[str,Any]:
     return json.loads(data.decode("utf-8"))
 
+# יצירת הסוקט שיאזין לבקשות המבקשות לקבל IP
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.bind(DHCP_IP, DHCP_PORT)
+    print(f"DHCP server listening on {DHCP_IP}:{DHCP_PORT}")
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        msg = decode(data)
+        print(f"DHCP Server recive from {addr}:{msg}")
