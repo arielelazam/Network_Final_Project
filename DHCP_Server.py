e66c1432e0e4639d06e8c55c31dbf287d50454e4
pinhaszanzuri <pinhas.zanzuri@msmail.ariel.ac.il>
2026-02-10 12:11:43 +0200

    encode/decode

--- DHCP_Server.py
+++ DHCP_Server.py
@@ -1,2 +1,13 @@
-print("you are banana")
-print("No, you are banana!")
+
+import json, socket
+from typing import Dict, Any
+
+
+# פונקציה שמקבלת דיקשנרי הופכת אותו לגיסון וממירה אותו לבתים
+def encode(msg: Dict[str,Any]) -> bytes:
+    return json.dumps(msg).encode("utf-8")
+
+# פונקציה שמקבלת בתים הופת אותם לגיסון וממירה אותם לדיקשנרי
+def decode(data: bytes) -> dict[str,Any]:
+    return json.loads(data.decode("utf-8"))
+
