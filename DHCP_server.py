6174da58cf0409b94bfcda4315a0ade7c6631cdc
Ariel Elazam <arielelazam123@gmail.com>
2026-03-09 19:47:27 +0200

    Add main function to DHCP

--- DHCP_server.py
+++ DHCP_server.py
@@ -9,13 +9,10 @@ SUBNET_MASK = "255.255.255.0"
 OFFER_TIMEOUT = 10 # הזמן שנתמש בו בשביל הקצאות
 LEASE_TIME = 600 # הזמן המוקצה ללקוח לשימוש בכתובת IP (שניות)
 
-ip_to_client: Dict[str, int] = {} # מבנה הנתונים המחזיק: מפתח = כתובת IP כלשהי, וערך = ה-ID של הלקוח המשתמש בה
-
 client_name_to_id: Dict[str, int] = {} # מבנה נתונים המחזיק: מפתח = שם הלקוח (שאיתו הוא מזדהה), וערך = ה-ID שהוא קיבל
+ip_to_client: Dict[str, int] = {} # מבנה הנתונים המחזיק: מפתח = כתובת IP כלשהי, וערך = ה-ID של הלקוח המשתמש בה
 next_client_id = 1 # מונה לחלוקת מספרי ID
-
 pending_offers: Dict[int, tuple[str, float]] = {} # מבנה נתונים שיחזיק במפתח ID של משתמש ובערך יחזיק את ה-IP שהצענו לו ואת הזמן המוקצה לו
-
 ip_leases: Dict[str, float] = {} # מבנה נתונים המחזיק במפתח כתובת IP שנמצאת בשימוש ובערך את הזמן שנותר לה להיות בשימוש
 
 # פונקציה המנקה הצעות שפג תוקפן
@@ -76,206 +73,214 @@ def ip_available_for_client(requested_ip: str, client_id: int) -> bool:
     owner_id = ip_to_client.get(requested_ip)
     return owner_id is None or owner_id == client_id
 
-# יצירת הסוקט שיאזין לבקשות המבקשות לקבל IP
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.bind((DHCP_IP,DHCP_PORT))
-    sock.settimeout(1.0)
-    print(f"DHCP server listening on {DHCP_IP}:{DHCP_PORT}")
+def main():
 
-    while True:
-        cleanup_expired_offers() # ביצוע ניקוי להצעות שפג תוקפן
-        cleanup_expired_leases() # ביצוע ניקוי לכתובות שפג תוקפן והחזרתן למאגר
-        try:
-            data, addr = sock.recvfrom(4096)
-            msg = decode(data)
-        except socket.timeout:
-            continue
-        except json.JSONDecodeError:
-            print(f"SERVER got invalid JSON: {data!r}")
-            continue
+    global next_client_id # נבצע שינוי על משתנה בסקופ גלובלי ולכן נוסיף global
 
-        print(f"SERVER receive from {addr}: {msg}")
+    # יצירת הסוקט שיאזין לבקשות המבקשות לקבל IP
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.bind((DHCP_IP, DHCP_PORT))
+        sock.settimeout(1.0)
+        print(f"DHCP server listening on {DHCP_IP}:{DHCP_PORT}")
 
-        if msg.get("type") == "DHCP_DISCOVER": # אם הלקוח מבקש לקבל כתובת IP כלשהי
-            client_name = msg.get("client_name") # נשמור את שם הלקוח
+        while True:
+            cleanup_expired_offers()  # ביצוע ניקוי להצעות שפג תוקפן
+            cleanup_expired_leases()  # ביצוע ניקוי לכתובות שפג תוקפן והחזרתן למאגר
+            try:
+                data, addr = sock.recvfrom(4096)
+                msg = decode(data)
+            except socket.timeout:
+                continue
+            except json.JSONDecodeError:
+                print(f"SERVER got invalid JSON: {data!r}")
+                continue
 
-            # אם לא התקבל שם משתמש כמחרוזת או שלא התקבל בכלל נחזיר הודעת שגיאה ללקוח
-            if not isinstance(client_name, str) or not client_name:
-                reply = {"type": "DHCP_NAK", "reason": "MISSING_CLIENT_NAME"}
+            print(f"SERVER receive from {addr}: {msg}")
+
+            if msg.get("type") == "DHCP_DISCOVER":  # אם הלקוח מבקש לקבל כתובת IP כלשהי
+                client_name = msg.get("client_name")  # נשמור את שם הלקוח
+
+                # אם לא התקבל שם משתמש כמחרוזת או שלא התקבל בכלל נחזיר הודעת שגיאה ללקוח
+                if not isinstance(client_name, str) or not client_name:
+                    reply = {"type": "DHCP_NAK", "reason": "MISSING_CLIENT_NAME"}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
+
+                # נבצע בדיקה האם שם הלקוח לא מחזיק בכתובת IP כלשהי ואם הוא לא, נקצה לו ID ונזכור שהוא מחזיק ב-ID הזה
+                if client_name not in client_name_to_id:
+                    client_name_to_id[client_name] = next_client_id
+                    next_client_id += 1
+
+                client_id = client_name_to_id[client_name]
+
+                # נחפש כתובת IP פנויה מהמאגר, אם לא מצאנו, נעדכן את הלקוח שאין
+                # אם מצאנו, נחזיר דיקשנרי המכיל את סוג הדיקשנרי, את ה-ID שנתנו ללקוח, את הכתובת המוצעת, ואת ה-subnet_mask
+                offered_ip = pick_free_ip()
+                if offered_ip is None:
+                    reply = {"type": "DHCP_NAK", "reason": "NO_FREE_IP", "client_id": client_id}
+                else:
+                    pending_offers[client_id] = (offered_ip, time.time() + OFFER_TIMEOUT)
+                    reply = {
+                        "type": "DHCP_OFFER",
+                        "client_id": client_id,
+                        "offered_ip": offered_ip,
+                        "subnet_mask": SUBNET_MASK,
+                        "offer_timeout": OFFER_TIMEOUT
+                    }
+
                 sock.sendto(encode(reply), addr)
                 print(f"SERVER sent to {addr}: {reply}")
-                continue
-
-            #נבצע בדיקה האם שם הלקוח לא מחזיק בכתובת IP כלשהי ואם הוא לא, נקצה לו ID ונזכור שהוא מחזיק ב-ID הזה
-            if client_name not in client_name_to_id:
-                client_name_to_id[client_name] = next_client_id
-                next_client_id += 1
-
-            client_id = client_name_to_id[client_name]
-
-            # נחפש כתובת IP פנויה מהמאגר, אם לא מצאנו, נעדכן את הלקוח שאין
-            # אם מצאנו, נחזיר דיקשנרי המכיל את סוג הדיקשנרי, את ה-ID שנתנו ללקוח, את הכתובת המוצעת, ואת ה-subnet_mask
-            offered_ip = pick_free_ip()
-            if offered_ip is None:
-                reply = {"type": "DHCP_NAK", "reason": "NO_FREE_IP", "client_id": client_id}
-            else:
-                pending_offers[client_id] = (offered_ip, time.time() + OFFER_TIMEOUT)
-                reply = {
-                    "type": "DHCP_OFFER",
-                    "client_id": client_id,
-                    "offered_ip": offered_ip,
-                    "subnet_mask": SUBNET_MASK,
-                    "offer_timeout": OFFER_TIMEOUT
-                }
-
-            sock.sendto(encode(reply), addr)
-            print(f"SERVER sent to {addr}: {reply}")
 
 
-        elif msg.get("type") == "DHCP_REQUEST": # אם הלקוח מעוניין לקבל את הכתובת
-            client_name = msg.get("client_name") # נשמור את שם הלקוח
-            requested_ip = msg.get("requested_ip") # נשמור את כתובת ה-IP שהוא מעוניין לקבל
+            elif msg.get("type") == "DHCP_REQUEST":  # אם הלקוח מעוניין לקבל את הכתובת
+                client_name = msg.get("client_name")  # נשמור את שם הלקוח
+                requested_ip = msg.get("requested_ip")  # נשמור את כתובת ה-IP שהוא מעוניין לקבל
 
-            # אם הלקוח שלח שם לא תקין או לא שלח שם נחזיר הודעת שגיאה
-            if not isinstance(client_name, str) or not client_name:
-                reply = {"type": "DHCP_NAK", "reason": "MISSING_CLIENT_NAME"}
-                sock.sendto(encode(reply), addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
+                # אם הלקוח שלח שם לא תקין או לא שלח שם נחזיר הודעת שגיאה
+                if not isinstance(client_name, str) or not client_name:
+                    reply = {"type": "DHCP_NAK", "reason": "MISSING_CLIENT_NAME"}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
 
-            # אם שם הלקוח לא במאגר השמות נחזיר שגיאה
-            if client_name not in client_name_to_id:
-                reply = {"type": "DHCP_NAK", "reason": "UNKNOWN_CLIENT"}
-                sock.sendto(encode(reply), addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
+                # אם שם הלקוח לא במאגר השמות נחזיר שגיאה
+                if client_name not in client_name_to_id:
+                    reply = {"type": "DHCP_NAK", "reason": "UNKNOWN_CLIENT"}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
 
-            client_id = client_name_to_id[client_name]  # נשמור את ה-ID של הלקוח
+                client_id = client_name_to_id[client_name]  # נשמור את ה-ID של הלקוח
 
-            # אם הכתובת המבוקשת לא חוקית או לא קיימת, נחזיר שגיאה
-            if not isinstance(requested_ip, str) or not requested_ip:
-                reply = {"type": "DHCP_NAK", "reason": "MISSING_REQUESTED_IP"}
-                sock.sendto(encode(reply), addr)
-                print(f"[SERVER] sent to {addr}: {reply}")
-                continue
+                # אם הכתובת המבוקשת לא חוקית או לא קיימת, נחזיר שגיאה
+                if not isinstance(requested_ip, str) or not requested_ip:
+                    reply = {"type": "DHCP_NAK", "reason": "MISSING_REQUESTED_IP"}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
 
-            # אם הכתובת המבוקשת היא לא אחת מהכתובות האפשרויות, נחזיר שגיאה
-            if requested_ip not in POOL:
-                reply = {"type": "DHCP_NAK", "reason": "IP_NOT_IN_POOL", "client_id": client_id}
-                sock.sendto(encode(reply), addr)
-                print(f"[SERVER] sent to {addr}: {reply}")
-                continue
+                # אם הכתובת המבוקשת היא לא אחת מהכתובות האפשרויות, נחזיר שגיאה
+                if requested_ip not in POOL:
+                    reply = {"type": "DHCP_NAK", "reason": "IP_NOT_IN_POOL", "client_id": client_id}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
 
-            offer = pending_offers.get(client_id)
-            # אם לא שוריינה ללקוח אף כתובת, נחזיר שגיאה
-            if offer is None:
-                reply = {"type": "DHCP_NAK", "reason": "NO_PENDING_OFFER", "client_id": client_id}
-                sock.sendto(encode(reply),addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
+                offer = pending_offers.get(client_id)
+                # אם לא שוריינה ללקוח אף כתובת, נחזיר שגיאה
+                if offer is None:
+                    reply = {"type": "DHCP_NAK", "reason": "NO_PENDING_OFFER", "client_id": client_id}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
 
-            offered_ip, expiry_time = offer
-            # אם הלקוח מעוניין בכתובת אבל פג הזמן, נחזיר שגיאה
-            if time.time() > expiry_time:
-                del pending_offers[client_id]
-                reply = {"type": "DHCP_NAK", "reason": "OFFER_EXPIRED", "client_id": client_id}
-                sock.sendto(encode(reply), addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
+                offered_ip, expiry_time = offer
+                # אם הלקוח מעוניין בכתובת אבל פג הזמן, נחזיר שגיאה
+                if time.time() > expiry_time:
+                    del pending_offers[client_id]
+                    reply = {"type": "DHCP_NAK", "reason": "OFFER_EXPIRED", "client_id": client_id}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
 
-            # אם הלקוח מעוניין לקבל כתובת אחרת ממה שהצענו לו, נחזיר שגיאה
-            if requested_ip != offered_ip:
-                del pending_offers[client_id]
-                reply = {"type": "DHCP_NAK", "reason": "REQUEST_NOT_MATCH_OFFER", "client_id": client_id}
-                sock.sendto(encode(reply), addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
+                # אם הלקוח מעוניין לקבל כתובת אחרת ממה שהצענו לו, נחזיר שגיאה
+                if requested_ip != offered_ip:
+                    del pending_offers[client_id]
+                    reply = {"type": "DHCP_NAK", "reason": "REQUEST_NOT_MATCH_OFFER", "client_id": client_id}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
 
-           # אם הלקוח לא רשאי לקבל את הכתובת שהוא מבקש (למשל אם היא שייכת כבר למישהו אחר), נחזיר שגיאה
-            if not ip_available_for_client(requested_ip, client_id):
-                del pending_offers[client_id]
-                reply = {"type": "DHCP_NAK", "reason": "IP_TAKEN", "client_id": client_id}
-                sock.sendto(encode(reply), addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
+                # אם הלקוח לא רשאי לקבל את הכתובת שהוא מבקש (למשל אם היא שייכת כבר למישהו אחר), נחזיר שגיאה
+                if not ip_available_for_client(requested_ip, client_id):
+                    del pending_offers[client_id]
+                    reply = {"type": "DHCP_NAK", "reason": "IP_TAKEN", "client_id": client_id}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
 
-            ip_to_client[requested_ip] = client_id # נאשר למשתמש לקבל את כתובת ה-IP שהוא ביקש ונגדיר שהיא שייכת לו
-            ip_leases[requested_ip] = time.time() + LEASE_TIME # נשמור את הזמן שיש למשתמש עם כתובת ה-IP שנתנו לו
-            del pending_offers[client_id] # נמחק אותו מרשימת הבקשות הממתינות
+                ip_to_client[requested_ip] = client_id  # נאשר למשתמש לקבל את כתובת ה-IP שהוא ביקש ונגדיר שהיא שייכת לו
+                ip_leases[requested_ip] = time.time() + LEASE_TIME  # נשמור את הזמן שיש למשתמש עם כתובת ה-IP שנתנו לו
+                del pending_offers[client_id]  # נמחק אותו מרשימת הבקשות הממתינות
 
-            # נחזיר ללקוח הודעת ACK המאשרת לו שהוא קיבל את הכתובת, יתר הנתונים ואת הזמן שיש לו להשתמש בכתובת (בשניות)
-            reply = {
-                "type": "DHCP_ACK",
-                "client_id": client_id,
-                "your_ip": requested_ip,
-                "subnet_mask": SUBNET_MASK,
-                "lease_seconds": LEASE_TIME
-            }
-            sock.sendto(encode(reply), addr)
-            print(f"SERVER sent to {addr}: {reply}")
-
-        # הלקוח מבקש לחדש את הכתובת שכבר יש לו
-        elif msg.get("type") == "DHCP_RENEW":
-            client_name = msg.get("client_name")
-            current_ip = msg.get("current_ip")
-
-            # אם אין שם של לקוח או שזה לא תיקני, לא נאפשר חידוש
-            if not isinstance(client_name, str) or not client_name:
-                reply = {"type": "DHCP_NAK", "reason": "MISSING_CLIENT_NAME"}
-                sock.sendto(encode(reply), addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
-
-            #  אם השרת לא מכיר את שם הלקוח הזה, לא נאפשר חידוש
-            if client_name not in client_name_to_id:
-                reply = {"type": "DHCP_NAK", "reason": "UNKNOWN_CLIENT"}
-                sock.sendto(encode(reply), addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
-
-            # אם הלקוח לא שלח IP לחידוש
-            if not isinstance(current_ip, str) or not current_ip:
-                reply = {"type": "DHCP_NAK", "reason": "MISSING_CURRENT_IP"}
-                sock.sendto(encode(reply), addr)
-                print(f"SERVER sent to {addr}: {reply}")
-                continue
-
-            # ה- ID של שם הלקוח
-            client_id = client_name_to_id[client_name]
-
-            # ה- ID של ה- IP הנוכחי
-            owner_id = ip_to_client.get(current_ip)
-
-            #לוקחים את זמן פקיעת ה-lease הנוכחי של ה-IP
-            lease_exp = ip_leases.get(current_ip)
-
-            now = time.time()
-
-            # אם זה אותו בעלים וגם קיים זמן והוא לא פג - מאריכים לו את הזמן
-            if owner_id == client_id and lease_exp is not None and lease_exp > now:
-                ip_leases[current_ip] = now + LEASE_TIME
-
+                # נחזיר ללקוח הודעת ACK המאשרת לו שהוא קיבל את הכתובת, יתר הנתונים ואת הזמן שיש לו להשתמש בכתובת (בשניות)
                 reply = {
                     "type": "DHCP_ACK",
                     "client_id": client_id,
-                    "your_ip": current_ip,
+                    "your_ip": requested_ip,
                     "subnet_mask": SUBNET_MASK,
-                    "lease_seconds": LEASE_TIME,
-                    "renewed": True # דגל עזר שמסמן ללקוח שזה ACK של חידוש
+                    "lease_seconds": LEASE_TIME
                 }
+                sock.sendto(encode(reply), addr)
+                print(f"SERVER sent to {addr}: {reply}")
 
+            # הלקוח מבקש לחדש את הכתובת שכבר יש לו
+            elif msg.get("type") == "DHCP_RENEW":
+                client_name = msg.get("client_name")
+                current_ip = msg.get("current_ip")
+
+                # אם אין שם של לקוח או שזה לא תיקני, לא נאפשר חידוש
+                if not isinstance(client_name, str) or not client_name:
+                    reply = {"type": "DHCP_NAK", "reason": "MISSING_CLIENT_NAME"}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
+
+                #  אם השרת לא מכיר את שם הלקוח הזה, לא נאפשר חידוש
+                if client_name not in client_name_to_id:
+                    reply = {"type": "DHCP_NAK", "reason": "UNKNOWN_CLIENT"}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
+
+                # אם הלקוח לא שלח IP לחידוש
+                if not isinstance(current_ip, str) or not current_ip:
+                    reply = {"type": "DHCP_NAK", "reason": "MISSING_CURRENT_IP"}
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
+
+                # ה- ID של שם הלקוח
+                client_id = client_name_to_id[client_name]
+
+                # ה- ID של ה- IP הנוכחי
+                owner_id = ip_to_client.get(current_ip)
+
+                # לוקחים את זמן פקיעת ה-lease הנוכחי של ה-IP
+                lease_exp = ip_leases.get(current_ip)
+
+                now = time.time()
+
+                # אם זה אותו בעלים וגם קיים זמן והוא לא פג - מאריכים לו את הזמן
+                if owner_id == client_id and lease_exp is not None and lease_exp > now:
+                    ip_leases[current_ip] = now + LEASE_TIME
+
+                    reply = {
+                        "type": "DHCP_ACK",
+                        "client_id": client_id,
+                        "your_ip": current_ip,
+                        "subnet_mask": SUBNET_MASK,
+                        "lease_seconds": LEASE_TIME,
+                        "renewed": True  # דגל עזר שמסמן ללקוח שזה ACK של חידוש
+                    }
+
+                    sock.sendto(encode(reply), addr)
+                    print(f"SERVER sent to {addr}: {reply}")
+                    continue
+
+                # אם לא עמד בתנאי חידוש -> דוחים ודורשים תהליך DHCP מחדש
+                reply = {"type": "DHCP_NAK", "reason": "LEASE_EXPIRED_RESTART_DHCP"}
                 sock.sendto(encode(reply), addr)
                 print(f"SERVER sent to {addr}: {reply}")
                 continue
 
-            # אם לא עמד בתנאי חידוש -> דוחים ודורשים תהליך DHCP מחדש
-            reply = {"type": "DHCP_NAK", "reason": "LEASE_EXPIRED_RESTART_DHCP"}
-            sock.sendto(encode(reply), addr)
-            print(f"SERVER sent to {addr}: {reply}")
-            continue
+
+            else:
+                reply = {"type": "DHCP_NAK", "reason": "UNKNOWN_MESSAGE_TYPE"}
+                sock.sendto(encode(reply), addr)
+                print(f"SERVER sent to {addr}: {reply}")
 
 
-        else:
-            reply = {"type": "DHCP_NAK", "reason": "UNKNOWN_MESSAGE_TYPE"}
-            sock.sendto(encode(reply), addr)
-            print(f"SERVER sent to {addr}: {reply}")
+if __name__ == "__main__":
+    main()
