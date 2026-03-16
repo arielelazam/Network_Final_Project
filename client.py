1abb46b6a8f7a623926b1c403265eacca354f482
Ariel Elazam <arielelazam123@gmail.com>
2026-03-16 15:52:24 +0200

    Update the menu message in client

--- client.py
+++ client.py
@@ -498,7 +498,7 @@ def main():
                     return
                 next_renew_time = time.time() + 540
 
-        user_domain = input("Enter some Domain name (app.local for the application), or 'exit':\n").strip()
+        user_domain = input("Enter some Domain name (app.local for the application) or 'exit':\n").strip()
 
         if user_domain.lower() == "exit":
             print("GoodBye! -> Connection closed")
